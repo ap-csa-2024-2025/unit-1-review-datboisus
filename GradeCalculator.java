dd5100902cce4262b1db93d573a6b876e7d19e5a
datboisus <donvu251106@gmail.com>
2024-09-16 10:17:44 -0700

    1

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,7 +1,28 @@
 public class GradeCalculator
 {
   public static void main(String[] args)
-  {
-    // insert code here
-  }
+{
+    String Course = "AP Comp Sci";
+    int aveTime = 175;
+    int Hw1 = 89;
+    int Hw2 = 78;
+    int Hw3 = 72;
+    int Hw4 = 83;
+    double Q1 = 89.48;
+    double Q2 = 52.54;
+    double F1 = 11.54;
+
+    int weekTime1 = aveTime / 60;
+    int weekTime2 = aveTime % 60;
+    double aveHw = (double) Hw1 + Hw2 + Hw3 + Hw4 / ;
+    
+  
+    
+    
+    
+    
+    
+    System.out.println("Weekly time spent: " + weekTime1 + "h" + weekTime2);
+    System.out.println("Average homework grade: " + aveHw);
+}
 }
